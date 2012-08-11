f844283097786f96024a134d49bd2e0811cf4074
CFPS <christian.f.p.scholz@gmail.com>
2012-08-11 20:52:29 +0200

    updateret homopolymer.py

--- lib/homopolymer.py
+++ lib/homopolymer.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python -u
 # -*- coding: latin-1 -*-
+def find(mappe, cpu=6, minLengde=5):
+    import os, time
+    from multiprocessing import Process, Queue, active_children
+    arbejdsliste = os.listdir(mappe)
+    while arbejdsliste and len(active_children()):
+        while len(active_children()) < cpu:
+            #spawn child
+        time.sleep(1)
+    print 'Færdig med arbejdet!'
+    return
 
+def child(fil, mappe):
+    #indlæs filen
+    udfil = open(mappe +'/homopolymer-'+fil, 'wb')
+    for sekvens in filen:
+        for base in ('A', 'T', 'C', 'G'):
+            ind = 0
+            while 1:
+                try:
+                    homopolymer = sekvens[ind:].index(base)
+                    udfil.write('>|' + str(homopolymer) + '|\n' + sekvens[homopolymer-100:homopolymer+100])
+                    ind = homopolymer + 100
+                except ValueError:
+                    break
+    udfil.close()
+    return
 
 if __name__ == "__main__":
     import sys
-    hentRAST(sys.argv[1])
+    find(sys.argv[1])
+    exit()
